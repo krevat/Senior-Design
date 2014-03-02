573a5f29a043d8f03a55d93b4860ceff7e0e11a6
Rina Krevat <krevat@seas.upenn.edu>
2014-03-02 14:16:47 -0500

    working FMOD
    a working console app to manipulate sound!

---
+++ playstream/main.cpp
@@ -0,0 +1,249 @@
+/*===============================================================================================
+ PlayStream Example
+ Copyright (c), Firelight Technologies Pty, Ltd 2004-2014.
+
+ This example shows how to simply play a stream, such as an mp3 or wav.
+ The stream behaviour is achieved by specifying FMOD_CREATESTREAM in the call to 
+ System::createSound.
+ This makes FMOD decode the file in realtime as it plays, instead of loading it all at once.
+ This uses far less memory, in exchange for a small runtime cpu hit.
+===============================================================================================*/
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+#include "fmod.hpp"
+#include "fmod_errors.h"
+
+void ERRCHECK(FMOD_RESULT result)
+{
+    if (result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        exit(-1);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    FMOD::System     *system;
+    FMOD::Sound      *sound;
+    FMOD::Channel    *channel = 0;
+    FMOD_RESULT       result;
+    int               key;
+    unsigned int      version;
+	// rina
+	FMOD::Sound *stream1;
+	FMOD::DSP *pitch;
+	float defreq, spfreq, pit;
+	bool done = false, active = true;
+
+    /*
+        Create a System object and initialize.
+    */
+    result = FMOD::System_Create(&system);
+    ERRCHECK(result);
+
+    result = system->getVersion(&version);
+    ERRCHECK(result);
+
+    if (version < FMOD_VERSION)
+    {
+        printf("Error!  You are using an old version of FMOD %08x.  This program requires %08x\n", version, FMOD_VERSION);
+        return 0;
+    }
+
+    result = system->init(1, FMOD_INIT_NORMAL, 0);
+    ERRCHECK(result);
+
+    //result = system->createStream("../media/wave.mp3", FMOD_HARDWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &sound);
+    //result = system->createStream("C:/Users/rina/Downloads/AllegroNonTroppo.mp3", FMOD_HARDWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &sound);
+	result = system->createStream("C:/Users/rina/Downloads/flowers-midi.mid", FMOD_HARDWARE | FMOD_LOOP_NORMAL | FMOD_2D, 0, &sound);
+	ERRCHECK(result);
+
+    printf("====================================================================\n");
+    printf("PlayStream Example.  Copyright (c) Firelight Technologies 2004-2014.\n");
+    printf("====================================================================\n");
+    printf("\n");
+    printf("Press space to pause, Esc to quit\n");
+    printf("\n");
+	printf("Press 'v'/'V' to change channel volume\n");
+	printf("Press 'f'/'F' to change channel frequency\n");
+	printf("Press '['/']' to change channel pan\n");
+	printf("Press 's'/'S' to change channel speed (tempo)\n");
+	printf("\n");
+
+    /*
+        Play the sound.
+    */
+
+    result = system->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
+    ERRCHECK(result);
+
+    /*
+        Main loop.
+    */
+    do
+    {
+        if (_kbhit())
+        {
+            key = _getch();
+
+            switch (key)
+            {
+                case ' ' :
+                {
+                    bool paused;
+                    channel->getPaused(&paused);
+                    channel->setPaused(!paused);
+                    break;
+                }
+                case 'p' : 
+                { 
+                    FMOD::ChannelGroup* masterGroup = NULL; 
+                    system->getMasterChannelGroup(&masterGroup); 
+                    if (masterGroup) 
+                    { 
+                        bool paused; 
+                        masterGroup->getPaused(&paused); 
+                        masterGroup->setPaused(!paused); 
+                    } 
+                    break; 
+                }
+				// this is where i started to do stuff
+				case 'v' :
+                {
+                    float volume;
+
+                    channel->getVolume(&volume);
+                    volume -= 0.1f;
+                    channel->setVolume(volume);
+                    break;
+                }
+                case 'V' :
+                {
+                    float volume;
+
+                    channel->getVolume(&volume);
+                    volume += 0.1f;
+                    channel->setVolume(volume);
+                    break;
+                }
+				case 'f':
+				{
+					float frequency;
+
+                    channel->getFrequency(&frequency);
+                    frequency -= 500.0f;
+                    channel->setFrequency(frequency);
+					break;
+				}
+				case 'F' :
+                {
+                    float frequency;
+
+                    channel->getFrequency(&frequency);
+                    frequency += 500.0f;
+                    channel->setFrequency(frequency);
+                    break;
+                }
+				case '[' :
+                {
+                    float pan;
+
+                    channel->getPan(&pan);
+                    pan -= 0.1f;
+                    channel->setPan(pan);
+                    break;
+                }
+                case ']' :
+                {
+                    float pan;
+
+                    channel->getPan(&pan);
+                    pan += 0.1f;
+                    channel->setPan(pan);
+                    break;
+                }
+
+
+				case 's' :
+				{
+
+					float thesound;
+					sound->getMusicSpeed(&thesound);
+					sound->setMusicSpeed(thesound-0.1f);
+
+						break;
+					}
+
+				case 'S' :
+				{
+
+					float thesound;
+					sound->getMusicSpeed(&thesound);
+					sound->setMusicSpeed(thesound+0.1f);
+
+						break;
+				}
+				// end rina
+            }
+        }
+
+        system->update();
+
+        if (channel)
+        {
+            unsigned int ms;
+            unsigned int lenms;
+            bool         playing;
+            bool         paused;
+
+            result = channel->isPlaying(&playing);
+            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE))
+            {
+                ERRCHECK(result);
+            }
+
+            result = channel->getPaused(&paused);
+            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE))
+            {
+                ERRCHECK(result);
+            }
+
+            result = channel->getPosition(&ms, FMOD_TIMEUNIT_MS);
+            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE))
+            {
+                ERRCHECK(result);
+            }
+
+            result = sound->getLength(&lenms, FMOD_TIMEUNIT_MS);
+            if ((result != FMOD_OK) && (result != FMOD_ERR_INVALID_HANDLE))
+            {
+                ERRCHECK(result);
+            }
+
+            printf("Time %02d:%02d:%02d/%02d:%02d:%02d : %s\r", ms / 1000 / 60, ms / 1000 % 60, ms / 10 % 100, lenms / 1000 / 60, lenms / 1000 % 60, lenms / 10 % 100, paused ? "Paused " : playing ? "Playing" : "Stopped");
+		
+		
+		}
+
+        Sleep(10);
+
+    } while (key != 27);
+
+    printf("\n");
+
+    /*
+        Shut down
+    */
+    result = sound->release();
+    ERRCHECK(result);
+    result = system->close();
+    ERRCHECK(result);
+    result = system->release();
+    ERRCHECK(result);
+
+    return 0;
+}
