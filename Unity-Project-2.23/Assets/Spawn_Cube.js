3b0ccb942a59b0d537487c522c4cf6c991dd23d4
Rina Krevat <krevat@seas.upenn.edu>
2014-03-08 20:16:44 -0500

    some unity attempts

---
+++ Unity-Project-2.23/Assets/Spawn_Cube.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var cube : GameObject;
+var spawn_position;
+//var timer = 0.0;
+
+function spawn_cube () {
+	spawn_position = Vector3(0,0,0);
+	var temp_spawn_cube = Instantiate(cube, spawn_position, Quaternion.identity);
+	Destroy(temp_spawn_cube, 1.0);
+}
+
+function Start () {
+
+}
+
+function Update () {
+	//timer += Time.deltaTime;
+//	if (timer > 5) {
+	if(Input.GetKey("s")) {
+		spawn_cube();
+		//timer = 0.0;
+	}
+}
---
+++ Unity-Project-2.23/Assets/mainScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+	var objCube:GameObject;
+
+function Start () {
+	objCube.transform.position.x = 0;
+	objCube.transform.position.z = 0;
+}
+
+function Update () {
+	if (Input.GetKey("up")) { 
+		objCube.transform.position.z += 1;
+	}
+	if (Input.GetKey("down")) { 
+		objCube.transform.position.z -= 1;
+	}
+	if (Input.GetKey("left")) { 
+		objCube.transform.position.x -= 1;
+	}
+	if (Input.GetKey("right")) { 
+		objCube.transform.position.x += 1;
+	}
+	if (Input.GetKey("r")) { 
+		objCube.transform.position.x = 0;
+		objCube.transform.position.z = 0;
+	}
+}
